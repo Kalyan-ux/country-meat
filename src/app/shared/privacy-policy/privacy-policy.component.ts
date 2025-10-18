435efab4a975a7e50eb8b6ff9cf706479989638d
ahmed <ahmedtasleem296699@gmail.com>
2025-10-18 16:21:21 +0530

    Updated privacy policy

--- src/app/shared/privacy-policy/privacy-policy.component.ts
+++ src/app/shared/privacy-policy/privacy-policy.component.ts
@@ -1,30 +1,19 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-=======
->>>>>>> updates
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './privacy-policy.component.html',
-<<<<<<< HEAD
-  styleUrl: './privacy-policy.component.scss',
+  styleUrls: ['./privacy-policy.component.scss'],
 })
 export class PrivacyPolicyComponent {
   constructor(private router: Router) {}
 
-  goHome() {
+  goHome(): void {
     this.router.navigate(['/']);
   }
-}
-  styleUrls: ['./privacy-policy.component.scss']
-=======
-  styleUrl: './privacy-policy.component.scss'
->>>>>>> updates
-})
-export class PrivacyPolicyComponent {
-
 }
